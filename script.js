bb44f9c050e876022e50a8c1c7866981904cbea3
Swapna Sri Boggam <swapnasri784@gmail.com>
2025-10-23 09:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu on click
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
